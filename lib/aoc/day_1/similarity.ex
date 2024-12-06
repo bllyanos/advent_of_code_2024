b08e8b6a2603a792d7bbddc17fc27ec9b2a4d7e4
Billy Editiano <bllyanos@gmail.com>
2024-12-06 19:01:58 +0700

    chore: comments

--- lib/aoc/day_1/similarity.ex
+++ lib/aoc/day_1/similarity.ex
@@ -1,18 +1,11 @@
 defmodule AOC.Day1.Similarity do
+  # get the score of left list and right list
   def get_score({left_list, right_list} = _pairs) do
     occurences = calculate_occurences(%{}, right_list)
     left_list |> sum_score(occurences, 0)
   end
 
-  defp sum_score([entry | more], occur, acc) do
-    case Map.fetch(occur, entry) do
-      {:ok, occurence} -> sum_score(more, occur, acc + entry * occurence)
-      :error -> sum_score(more, occur, acc)
-    end
-  end
-
-  defp sum_score([], _, acc), do: acc
-
+  # calculate how many times each number from the left list appears in the right list.
   defp calculate_occurences(%{} = acc, [entry | more]) do
     case Map.fetch(acc, entry) do
       {:ok, current} -> Map.put(acc, entry, current + 1)
@@ -21,5 +14,17 @@ defmodule AOC.Day1.Similarity do
     |> calculate_occurences(more)
   end
 
+  # return the acc if no more entry
   defp calculate_occurences(%{} = acc, []), do: acc
+
+  # the score is determined by how many times each item in the first list appears in the second list, multiplied by itself.
+  defp sum_score([entry | more], occur, acc) do
+    case Map.fetch(occur, entry) do
+      {:ok, occurence} -> sum_score(more, occur, acc + entry * occurence)
+      :error -> sum_score(more, occur, acc)
+    end
+  end
+
+  # return the acc if no more entry
+  defp sum_score([], _, acc), do: acc
 end
